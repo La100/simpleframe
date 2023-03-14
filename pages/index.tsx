514559c5c7ea90a61f8738a4fdcbd3441e6cfe56
La100 <stolamarcin@gmail.com>
2023-03-14 12:12:41 +0100

    elo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
 import Header from '../components/header'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
